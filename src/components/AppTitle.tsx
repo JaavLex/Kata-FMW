bff4a4599b0ae5dd8734f1e3af683e9e86050ff2
JaavLex <alexandre.javet@hotmail.com>
2021-10-20 15:01:31 +0200

    [feat] Added an AppTitle Component with some tailwind styling

---
+++ src/components/AppTitle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type AppTitleProps = {
+	titleText: string;
+};
+
+export default function AppTitle({ titleText }: AppTitleProps) {
+	return (
+		<div
+			className="fixed top-0 items-center bg-gray-300 h-20 w-screen 
+								flex justify-center flex-row shadow-lg"
+		>
+			<h1 className="text-blue-400 text-center text-4xl font-bold">{titleText}</h1>
+		</div>
+	);
+}
