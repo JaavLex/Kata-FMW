f51e76c7c242f70a38d3918c02a96edc94363894
JaavLex <alexandre.javet@hotmail.com>
2021-10-21 13:40:33 +0200

    [feat] Button can be big or simple
    - Added a usestate for the player to start the game

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppButton from './AppButton';
 import AppTitle from './AppTitle';
 import QuizzBlock from './quizzBlock';
 
 function App() {
+	const [isPlaying, setIsPlaying] = useState(false);
 	return (
 		<div>
 			<AppTitle
@@ -12,8 +13,12 @@ function App() {
 			/>
 			<div className="flex justify-center items-center flex-col h-screen w-screen">
 				<div className="align-middle">
-					{true ? (
-						<AppButton buttonText="play" onClick={() => alert('You clicked it !')} />
+					{!isPlaying ? (
+						<AppButton
+							buttonText="play"
+							bigButton={true}
+							onClick={() => setIsPlaying(true)}
+						/>
 					) : (
 						<QuizzBlock starting={true}></QuizzBlock>
 					)}
--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -4,28 +4,46 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 type AppButtonProps = {
 	buttonText: string;
+	bigButton?: boolean;
 	onClick: () => void;
 };
 
-export default function AppButton({ buttonText, onClick }: AppButtonProps) {
+export default function AppButton({
+	buttonText,
+	bigButton = false,
+	onClick,
+}: AppButtonProps) {
 	return (
 		<div>
-			<button
-				className="text-white rounded-full h-80 w-80 bg-indigo-300 flex 
+			{bigButton ? (
+				<button
+					className="text-white rounded-full h-80 w-80 bg-indigo-300 flex 
                   flex-col justify-center items-center content-around transition 
                   duration-200 hover:shadow-2xl"
-				onClick={onClick}
-			>
-				{buttonText === 'play' ? (
-					<div className="text-9xl h-[128px] w-[128px]">
-						<FontAwesomeIcon icon={faPlay} color="white" />
-					</div>
-				) : (
-					<div className="text-6xl">
+					onClick={onClick}
+				>
+					{buttonText === 'play' ? (
+						<div className="text-9xl h-[128px] w-[128px]">
+							<FontAwesomeIcon icon={faPlay} color="white" />
+						</div>
+					) : (
+						<div className="text-6xl">
+							<p>{buttonText}</p>
+						</div>
+					)}
+				</button>
+			) : (
+				<button
+					className="text-white rounded-lg h-14 w-28 bg-indigo-300 flex 
+                  flex-row justify-center items-center content-around transition 
+                  duration-200 hover:shadow-2xl"
+					onClick={onClick}
+				>
+					<div className="text-3xl">
 						<p>{buttonText}</p>
 					</div>
-				)}
-			</button>
+				</button>
+			)}
 		</div>
 	);
 }
