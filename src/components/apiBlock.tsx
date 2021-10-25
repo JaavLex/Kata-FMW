48748b906eb5c9ec0c8408c134281f8d29f9cca6
JaavLex <alexandre.javet@hotmail.com>
2021-10-25 14:50:08 +0200

    [wip] Added the option to remove cards

--- src/components/apiBlock.tsx
+++ src/components/apiBlock.tsx
@@ -7,38 +7,55 @@ type ApiBlockProps = {
 };
 
 export default function ApiBlock({ starting = true }: ApiBlockProps) {
-	const [data, setData]: any = useState();
+	const [cardList, setCardList]: any = useState();
 	useEffect((): any => {
 		return fetch('https://api.fbi.gov/wanted/v1/list')
 			.then(response => response.ok && response.json())
 			.then(data => {
+				let tempData: any[] = [];
 				data.items.length = 8;
-				setData(data.items);
-				console.log(data.items);
+				data.items.forEach((e: any, i: any) => tempData.push({ id: i, info: e }));
+				setCardList(tempData);
+				console.log(cardList);
 			});
 	}, []);
+
+	function removeCard(cardId: number) {
+		const templList = [...cardList];
+		cardList.forEach((e: any, i: any) => e.id === cardId && templList.splice(i, 1));
+		setCardList(templList);
+	}
+
 	return (
 		<div className="flex flex-wrap justify-around content-between gap-11">
-			{data &&
-				data.map((item: any) => {
+			{cardList &&
+				cardList.map((item: any) => {
 					return (
 						<div
 							className="flex justify-start items-center flex-col bg-gray-discord 
                     border-solid rounded-lg w-96 h-96 overflow-hidden text-white"
 						>
-							<div className="flex justify-center items-center overflow-hidden w-full h-40">
+							<div className="overflow-hidden w-full h-40">
+								<button
+									className="bg-none outline-none w-2 h-2 font-bold absolute p-3"
+									onClick={() => removeCard(item.id)}
+								>
+									⤬
+								</button>
 								<img
-									src={item.images[0].original}
+									src={item.info.images[0].original}
 									className="object-center object-cover w-full"
 								/>
 							</div>
-							<h3 className="font-bold text-lg">{item.title}</h3>
-							<h4 className="text-red-600">→ {item.subjects[0]}</h4>
-							{item.description && <p className="text-center">{item.description}</p>}
-
-							<div className="flex justify-center items-center overflow-hidden w-full h-40"></div>
-							{item.reward_text && (
-								<p className="text-center text-yellow-100">{item.reward_text}</p>
+							<h3 className="font-bold text-lg">{item.info.title}</h3>
+							<h4 className="text-red-600">→ {item.info.subjects[0]}</h4>
+							{item.info.description && (
+								<p className="text-center">{item.info.description}</p>
+							)}
+							{item.info.reward_text && (
+								<p className="text-center text-yellow-100">
+									{item.info.reward_text}
+								</p>
 							)}
 						</div>
 					);
